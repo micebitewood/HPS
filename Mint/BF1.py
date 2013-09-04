a389434e108f0a342ea3434c94ec96cd51f9dfd7
micebitewood <mujiong@gmail.com>
2013-09-04 18:58:37 -0400

    it was not the right algorithm

--- Mint/BF1.py
+++ Mint/BF1.py
@@ -5,7 +5,7 @@ for denominations[1] in range(2, 97):
     for denominations[2] in range(denominations[1] + 1, 98):
         for denominations[3] in range(denominations[2] + 1, 99):
             for denominations[4] in range(denominations[3] + 1, 100):
-                arr = [0 for i in range(100)]
+                arr = [100 for i in range(100)]
                 for i in denominations:
                     arr[i - 1] = 1
                 score = 0
@@ -13,8 +13,8 @@ for denominations[1] in range(2, 97):
                     if arr[n - 1] != 1:
                         for i in denominations[::-1]:
                             if n - i > 0:
-                                arr[n - 1] = arr[n - 1 - i] + arr[i - 1]
-                                break
+                                if arr[n - 1] > arr[n - i - 1] + arr[i - 1]:
+                                    arr[n - 1] = arr[n - i - 1] + arr[i - 1]
                     if n % 5 == 0:
                         score += N * arr[n - 1]
                     else:
