e557e7978ede74d3f51a036f0624e66a3ab13354
micebitewood <mujiong@gmail.com>
2013-09-04 21:56:02 -0400

    re-factor

--- Mint/BF1.py
+++ Mint/BF1.py
@@ -4,7 +4,7 @@ def calc(denominations, left, right):
         for i in range(left, right + 1):
             calc(denominations + [i], i + 1, right + 1)
     else:
-        arr = [100 for i in range(100)]
+        arr = [100 for i in range(99)]
         for i in denominations:
             arr[i - 1] = 1
         score = 0
@@ -12,8 +12,9 @@ def calc(denominations, left, right):
             if arr[n - 1] != 1:
                 for i in denominations[::-1]:
                     if n - i > 0:
-                        if arr[n - 1] > arr[n - i - 1] + arr[i - 1]:
-                            arr[n - 1] = arr[n - i - 1] + arr[i - 1]
+                        coinNum = arr[n - i - 1] + arr[i - 1]
+                        if arr[n - 1] > coinNum:
+                            arr[n - 1] = coinNum
             if n % 5 == 0:
                 score += N * arr[n - 1]
             else:
@@ -23,7 +24,7 @@ def calc(denominations, left, right):
             minScore = score
 
 N = 1
+denominationsNum = 3
 minScore = 1000
-arr = [0 for i in range(100)]
-calc([1], 2, 100 - 5 + 1)
+calc([1], 2, 100 - denominationsNum + 1)
 print minScore
