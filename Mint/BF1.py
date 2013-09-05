1eb90bbf38df07281333e6ca8cc29dedca5b6c9a
micebitewood <mujiong@gmail.com>
2013-09-04 21:09:54 -0400

    re-factor code

--- Mint/BF1.py
+++ Mint/BF1.py
@@ -1,22 +1,29 @@
+def calc(denominations, left, right):
+    global maxScore
+    if right != 100:
+        for i in range(left, right + 1):
+            calc(denominations + [i], i + 1, right + 1)
+    else:
+        arr = [100 for i in range(100)]
+        for i in denominations:
+            arr[i - 1] = 1
+        score = 0
+        for n in range(1, 100):
+            if arr[n - 1] != 1:
+                for i in denominations[::-1]:
+                    if n - i > 0:
+                        if arr[n - 1] > arr[n - i - 1] + arr[i - 1]:
+                            arr[n - 1] = arr[n - i - 1] + arr[i - 1]
+            if n % 5 == 0:
+                score += N * arr[n - 1]
+            else:
+                score += arr[n - 1]
+        print denominations, score
+        if score < maxScore:
+            maxScore = score
+
 N = 1
-denominations = [1 for i in range(5)]
+maxScore = 1000
 arr = [0 for i in range(100)]
-for denominations[1] in range(2, 97):
-    for denominations[2] in range(denominations[1] + 1, 98):
-        for denominations[3] in range(denominations[2] + 1, 99):
-            for denominations[4] in range(denominations[3] + 1, 100):
-                arr = [100 for i in range(100)]
-                for i in denominations:
-                    arr[i - 1] = 1
-                score = 0
-                for n in range(1, 100):
-                    if arr[n - 1] != 1:
-                        for i in denominations[::-1]:
-                            if n - i > 0:
-                                if arr[n - 1] > arr[n - i - 1] + arr[i - 1]:
-                                    arr[n - 1] = arr[n - i - 1] + arr[i - 1]
-                    if n % 5 == 0:
-                        score += N * arr[n - 1]
-                    else:
-                        score += arr[n - 1]
-                print denominations, score
+calc([1], 2, 100 - 5 + 1)
+print maxScore
