507ee3102467b099321894bedd791fff5917db46
micebitewood <mujiong@gmail.com>
2013-09-05 15:25:48 -0400

    improve algorithm

--- Mint/BF1.py
+++ Mint/BF1.py
@@ -1,26 +1,42 @@
-def calc(denominations, left, right, minScore):
-    if right != 100:
-        for i in range(left, right + 1):
-            minScore = calc(denominations + [i], i + 1, right + 1, minScore)
-    else:
-        arr = [100 for i in range(99)]
+def getScore(denominations, right, exchangeNumberArr, score):
+    for n in range(denominations[-1] + 1, right):
         for i in denominations:
-            arr[i - 1] = 1
-        score = 0
-        for n in range(1, 100):
-            if arr[n - 1] != 1:
-                for i in denominations:
-                    if n - i > 0:
-                        arr[n - 1] = min(arr[n - 1], arr[n - i - 1] + 1)
-            if n % 5 == 0:
-                score += N * arr[n - 1]
-            else:
-                score += arr[n - 1]
-        minScore = min(score, minScore)
-        return minScore
+            exchangeNumberArr[n] = min(exchangeNumberArr[n], exchangeNumberArr[n - i] + 1)
+        if exchangeNumberArr[n] > maxNum:
+            return 0
+        if n % 5 == 0:
+            score += N * exchangeNumberArr[n]
+        else:
+            score += exchangeNumberArr[n]
+    return score
 
+def getMinScore(denominations, exchangeNumberArr, left, right, score, minScore):
+    if len(denominations) == denominationsNum:
+        newArr = exchangeNumberArr[:]
+        newScore = getScore(denominations, 100, newArr, score)
+        if newScore == 0:
+            return minScore
+        if newScore < minScore:
+            minScore = newScore
+            print denominations, newScore
+    else:
+        for i in range(left, right):
+            if i > maxNum * denominations[-1]:
+                return minScore
+            newArr = exchangeNumberArr[:]
+            newArr[i] = 1
+            newScore = getScore(denominations, i + 1, newArr, score)
+            if newScore == 0:
+                return minScore
+            newDenominations = denominations + [i]
+            minScore = getMinScore(newDenominations, newArr, i + 2, right + 2, newScore, minScore)
+    return minScore
+
+import time
+tic = time.clock()
 N = 4
 denominationsNum = 5
+maxNum = 5
 minScore = 4951
-calc([1], 2, 100 - denominationsNum + 1, minScore)
-print minScore
+getMinScore([1], [0, 1] + [100 for i in range(98)], 3, 100 - denominationsNum * 2 + 4, 1, minScore)
+print time.clock() - tic
