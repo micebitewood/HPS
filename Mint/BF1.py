d9766f66d286c0f2fdd0a0c2d9d648db6761a52a
micebitewood <mujiong@gmail.com>
2013-09-05 10:31:38 -0400

    re-factor min()

--- Mint/BF1.py
+++ Mint/BF1.py
@@ -1,8 +1,7 @@
-def calc(denominations, left, right):
-    global minScore
+def calc(denominations, left, right, minScore):
     if right != 100:
         for i in range(left, right + 1):
-            calc(denominations + [i], i + 1, right + 1)
+            minScore = calc(denominations + [i], i + 1, right + 1, minScore)
     else:
         arr = [100 for i in range(99)]
         for i in denominations:
@@ -10,21 +9,18 @@ def calc(denominations, left, right):
         score = 0
         for n in range(1, 100):
             if arr[n - 1] != 1:
-                for i in denominations[::-1]:
+                for i in denominations:
                     if n - i > 0:
-                        coinNum = arr[n - i - 1] + arr[i - 1]
-                        if arr[n - 1] > coinNum:
-                            arr[n - 1] = coinNum
+                        arr[n - 1] = min(arr[n - 1], arr[n - i - 1] + 1)
             if n % 5 == 0:
                 score += N * arr[n - 1]
             else:
                 score += arr[n - 1]
-        print denominations, score
-        if score < minScore:
-            minScore = score
+        minScore = min(score, minScore)
+        return minScore
 
-N = 1
-denominationsNum = 3
-minScore = 1000
-calc([1], 2, 100 - denominationsNum + 1)
+N = 4
+denominationsNum = 5
+minScore = 4951
+calc([1], 2, 100 - denominationsNum + 1, minScore)
 print minScore
