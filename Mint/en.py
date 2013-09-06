9d284fd40baac12a96c13e908f8c0c7dd6766b31
Jun Hee Lee <junheelee@nyu.edu>
2013-09-06 00:39:15 -0400

    added ecn/en bounds to improved exchange number problem - params need tweak

--- Mint/en.py
+++ Mint/en.py
@@ -1,18 +1,21 @@
-def calculateEn(denom, maxChange):
+def calculateEnSum(denom, maxChange, N, ecnLim, enLim):
 	numDenom = len(denom)
 	
-	ecn = [999999 for i in range(maxChange+101)]
+	ecn = [999999 for i in range(maxChange*2+1)]
 	ecn[0] = 0
 	
-	for i in range(1, maxChange+101):
-		if i >= 100:
-			ecn[i] = ecn[i-100]
+	for i in range(1, maxChange*2+1):
+		if i > maxChange:
+			ecn[i] = ecn[i-maxChange-1]
 		else:
 			for j in range(numDenom):
 				if i-denom[j] >= 0:
 					tmp = ecn[i-denom[j]]+1
 					if tmp < ecn[i]:
 						ecn[i] = tmp
+			
+			if ecn[i] > ecnLim:
+				return 999999
 	
 	en = ecn[:maxChange+1]
 	
@@ -21,23 +24,22 @@ def calculateEn(denom, maxChange):
 			tmp = ecn[i+j]+ecn[j]
 			if tmp < en[i]:
 				en[i] = tmp
+		
+		if en[i] > enLim:
+			return 999999
 	
-	return en
-
-def calculateSum(en, N):
 	return sum(en)+(N-1)*sum(en[::5])
 
-def getBestDenomEn(maxChange, N):
+def getBestDenomEn(maxChange, N, ecnLim, enLim):
 	enSumMin = 999999
 	
-	for i in range(2, maxChange/2-2):
-		for j in range(i+1, maxChange/2-1):
-			for k in range(j+1, maxChange/2):
-#				for l in range(k+1, maxChange/2+1):
+	for i in range(2, (maxChange+1)/2-1):
+		for j in range(i+1, (maxChange+1)/2):
+			for k in range(j+1, (maxChange+1)/2+1):
+#				for l in range(k+1, (maxChange+1)/2+2):
 #					denom = [1, i, j, k, l]
 					denom = [1, 5, i, j, k]
-					en = calculateEn(denom, maxChange)
-					enSum = calculateSum(en, N)
+					enSum = calculateEnSum(denom, maxChange, N, ecnLim, enLim)
 					
 					if enSum < enSumMin:
 						enSumMin = enSum
@@ -50,5 +52,7 @@ import time
 tic = time.clock()
 maxChange = 99
 N = 4
-getBestDenomEn(maxChange, N)
+ecnLim = 10
+enLim = 10
+getBestDenomEn(maxChange, N, ecnLim, enLim)
 print time.clock() - tic
