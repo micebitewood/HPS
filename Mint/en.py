5288a52d627b7ac8bd61754f2ce7c55046bccff5
Jun Hee Lee <junheelee9@gmail.com>
2013-09-05 21:53:30 -0400

    initial implementation for exchange number problem

---
+++ Mint/en.py
@@ -0,0 +1,51 @@
+def calculateEn(denom, maxChange):
+	numDenom = len(denom)
+	
+	ecn = [999999 for i in range(maxChange+101)]
+	ecn[0] = 0
+	
+	for i in range(1, maxChange+101):
+		if i >= 100:
+			ecn[i] = ecn[i-100]
+		else:
+			for j in range(numDenom):
+				if i-denom[j] >= 0:
+					tmp = ecn[i-denom[j]]+1
+					if tmp < ecn[i]:
+						ecn[i] = tmp
+	
+	en = ecn[:maxChange+1]
+	
+	for i in range(1, maxChange+1):
+		for j in range(1, maxChange+1):
+			tmp = ecn[i+j]+ecn[j]
+			if tmp < en[i]:
+				en[i] = tmp
+	
+	return en
+
+def calculateSum(en, N):
+	return sum(en)+(N-1)*sum(en[::5])
+
+def getBestDenomEn(maxChange, N):
+	enSumMin = 999999
+	
+	for i in range(2, maxChange/2-2):
+		for j in range(i+1, maxChange/2-1):
+			for k in range(j+1, maxChange/2):
+#				for l in range(k+1, maxChange/2+1):
+#					denom = [1, i, j, k, l]
+					denom = [1, 5, i, j, k]
+					en = calculateEn(denom, maxChange)
+					enSum = calculateSum(en, N)
+					
+					if enSum < enSumMin:
+						enSumMin = enSum
+						denomMin = denom
+						print enSumMin, denomMin
+
+	print enSumMin, denomMin
+
+maxChange = 99
+N = 4
+getBestDenomEn(maxChange, N)
