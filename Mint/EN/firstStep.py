fff8f92eb1193f5d5dd719554aaf1ddfe144ac20
micebitewood <mujiong@gmail.com>
2013-09-07 00:19:39 -0400

    solution of the second problem

---
+++ Mint/EN/firstStep.py
@@ -0,0 +1,46 @@
+def exchangeNumber(numberArr):
+    sortedList = [[]]
+    for i in range(1, len(numberArr)):
+        n = numberArr[i]
+        if len(sortedList) - 1 < n:
+            sortedList += [[]]
+        sortedList[n] += [i]
+    change = 0
+    for i in range(1, len(sortedList) - 1):
+        for j in range(1, len(sortedList) - i):
+            for price in sortedList[i]:
+                for change in sortedList[j]:
+                    if price > change:
+                        if i + j < numberArr[price - change]:
+                            change += 1
+                            numberArr[price - change] = i + j
+    sumFive = 0
+    sumOther = 0
+    for n in range(1, 100):
+        if n % 5 == 0:
+            sumFive += numberArr[n]
+        else:
+            sumOther += numberArr[n]
+    return (sumOther, sumFive)
+
+def calc(denominations, right, exactChangeNumberArr):
+    for n in range(denominations[-1] + 1, right):
+        for i in denominations:
+            exactChangeNumberArr[n] = min(exactChangeNumberArr[n], exactChangeNumberArr[n - i] + 1)
+
+def preCalc(denominations, exactChangeNumberArr):
+    if len(denominations) == numDenominations:
+        newArr = exactChangeNumberArr[:]
+        calc(denominations, 100, newArr)
+        (sumOther, sumFive) = exchangeNumber(newArr)
+        print denominations, sumOther, sumFive
+    else:
+        for i in range(denominations[-1] + 1, 96 + len(denominations)):
+            newArr = exactChangeNumberArr[:]
+            newArr[i] = 1
+            calc(denominations, i + 1, newArr)
+            newDenominations = denominations + [i]
+            preCalc(newDenominations, newArr)
+
+numDenominations = 5
+preCalc([1], [0, 1] + [100 for i in range(98)])
---
+++ Mint/EN/getMinEN.py
@@ -0,0 +1,17 @@
+key = []
+sum1 = []
+sum2 = []
+with open('selectedSums', 'r') as f:
+    for line in f:
+        temp = line.rsplit(' ', 2)
+        key.append(eval(temp[0]))
+        sum1.append(int(temp[1]))
+        sum2.append(int(temp[2]))
+import sys
+N = int(sys.argv[1])
+minScore = 9999
+for i in range(len(key)):
+    score = sum1[i] + N * sum2[i]
+    if score <= minScore:
+        minScore = score
+        print key[i], score
