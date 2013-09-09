a7704660a09828d318b34bfaf5e11bd4dae39f06
micebitewood <mujiong@gmail.com>
2013-09-09 10:36:10 -0400

    modify verify because we don't need N to be passed in as a parameter

--- Mint/EN/verify.py
+++ Mint/EN/verify.py
@@ -4,7 +4,7 @@ import sys
 denominations = [int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5])]
 arr = [100 for i in range(100)]
 arr[0] = 0
-N = int(sys.argv[6])
+N = 1
 for n in range(1, 100):
     if arr[n] != 1:
         for i in denominations:
