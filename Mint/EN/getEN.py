133b4fa7501dbd8ca25b4226528d8539d3ece683
Jun Hee Lee <junheelee@nyu.edu>
2013-09-08 23:27:36 -0400

    corrected getEN.py

--- Mint/EN/getEN.py
+++ Mint/EN/getEN.py
@@ -1,24 +1,26 @@
 def getEN(denom, maxChange, N):
 	numDenom = len(denom)
+	maxDenom = max(denom)
 	
-	ecn = [999999 for i in range(maxChange*2+1)]
+	ecn = [999999 for i in range(maxChange+1)]
 	ecn[0] = 0
+	lastUpdate = 0
+	i = 1
 	
-	for i in range(1, maxChange*2+1):
-		if i > maxChange:
-			ecn[i] = ecn[i-maxChange-1]
-		else:
-			for j in range(numDenom):
-				if i-denom[j] >= 0:
-					tmp = ecn[i-denom[j]]+1
-					if tmp < ecn[i]:
-						ecn[i] = tmp
+	while i <= lastUpdate+maxDenom:
+		for j in range(numDenom):
+			tmp = ecn[(i-denom[j])%100]+1
+			if tmp < ecn[i%100]:
+				ecn[i%100] = tmp
+				lastUpdate = i
+		i = i+1
+				
 	
 	en = ecn[:maxChange+1]
 	
 	for i in range(1, maxChange+1):
 		for j in range(1, maxChange+1):
-			tmp = ecn[i+j]+ecn[j]
+			tmp = ecn[(i+j)%100]+ecn[j]
 			if tmp < en[i]:
 				en[i] = tmp
 	
