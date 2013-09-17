2bf39ea79066474eb5894846e3f1a42d46a173fb
micebitewood <mujiong@gmail.com>
2013-09-17 13:20:48 -0400

    add a plotting program for TSP, implement a runnable version of modifiedMST.py

---
+++ TSP/plot.py
@@ -0,0 +1,11 @@
+import sys
+datafile = sys.argv[1]
+temp = []
+with open(datafile, 'r') as f:
+    for line in f:
+        temp.append(map(int, line.split()))
+
+import matplotlib.pyplot as plt
+plt.plot(temp[0], temp[1], '-')
+plt.axis([-100, 30100, -100, 30100])
+plt.show()
