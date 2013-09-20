aa1bc4b4bfb51e00dca6c60e5155f5a10f162df8
Jun Hee Lee <junheelee@nyu.edu>
2013-09-20 05:48:59 -0400

    improved sa with simple untangle operation and new random insert phase, highest score with tsp1000: 703700

--- TSP/siman.cpp
+++ TSP/siman.cpp
@@ -7,9 +7,11 @@ const int N = 1000;
 const char FILENAME_IN[] = "tsp1000";
 const char FILENAME_OUT[] = "output";
 
-const int NUM_TRIALS = 2;
-const int NUM_ITERATIONS = 10000; // 30000;
+const int NUM_TRIALS = 5;
+const int NUM_ITERATIONS_UNTANGLE = 30000;
+const int NUM_ITERATIONS_INSERT = 10000;
 const int NUM_STEPS = 10000;
+const int CUT = 4;
 
 const double TEMP_INIT = 0.005;
 const double TEMP_FINAL = 0.000001;
@@ -26,9 +28,12 @@ typedef struct {
 
 double d[N*N];	// Distance between two cities
 int mst[N-1];	// MST edges
+clock_t start;	// Timer start
+int tmp;
 
 inline int randRange(int r) { return rand() % r; }
-inline void swap(int &a, int &b) { int tmp = a; a = b; b = tmp; }
+inline void swap(int &a, int &b) { tmp = a; a = b; b = tmp; }
+inline double getElapsedTime() { return double(clock()-start)/CLOCKS_PER_SEC; }
 
 void readData() {
 	pos p[N];
@@ -44,9 +49,11 @@ void readData() {
 	fclose(in);
 	
 	// Pre-calculate Euclidean distances
-	for(int i=0; i<N; ++i)
-		for(int j=0; j<N; ++j)
-			d[i*N+j] = sqrt(pow(double(p[i].x-p[j].x), 2.0) + pow(double(p[i].y-p[j].y), 2.0) + pow(double(p[i].z-p[j].z), 2.0));
+	for(int i=0; i<N-1; ++i)
+		for(int j=i+1; j<N; ++j)
+			d[i*N+j] = d[j*N+i] = sqrt(pow(double(p[i].x-p[j].x), 2.0) + pow(double(p[i].y-p[j].y), 2.0) + pow(double(p[i].z-p[j].z), 2.0));
+	
+	printf("Done calculating distances! - %f\n", getElapsedTime());
 }
 
 void calculateMST() {
@@ -76,6 +83,8 @@ void calculateMST() {
 		mst[i] = e;
 		a[e%N] = true;
 	}
+	
+	printf("Done calculating MST! - %f\n", getElapsedTime());
 }
 
 void dfs(int node, int* count, bool f[N], int p[N]) {
@@ -125,7 +134,7 @@ void displayPath(int p[N]) {
 //	for(int i=0; i<N; ++i)
 //		printf("Travel from %d to %d: %f\n", p[i]+1, p[(i+1)%N]+1, d[p[i]*N+p[(i+1)%N]]);
 	
-	printf("Total travel distance: %f\n", calculateTravelDist(p));
+	printf("Total travel distance: %f - %f\n", calculateTravelDist(p), getElapsedTime());
 }
 
 void writePath(int p[N]) {
@@ -140,22 +149,62 @@ void writePath(int p[N]) {
 	fclose(out);
 }
 
+double untangle1(int p[N], double dist) {
+	double diff;
+	
+	for(int i=0; i<N; ++i) {
+		diff = d[p[i]*N+p[(i+2)%N]] + d[p[(i+1)%N]*N+p[(i+3)%N]] - d[p[i]*N+p[(i+1)%N]] - d[p[(i+2)%N]*N+p[(i+3)%N]];
+		
+		if(diff < 0.0) {
+			swap(p[(i+1)%N], p[(i+2)%N]);
+			dist += diff;
+		}
+	}
+	
+	return dist;
+}
+
+double untangle2(int p[N], double dist) {
+	double diff;
+	
+	for(int i=0; i<N; ++i) {
+		diff = d[p[i]*N+p[(i+3)%N]] + d[p[(i+1)%N]*N+p[(i+4)%N]] - d[p[i]*N+p[(i+1)%N]] - d[p[(i+3)%N]*N+p[(i+4)%N]];
+		
+		if(diff < 0.0) {
+			swap(p[(i+1)%N], p[(i+3)%N]);
+			dist += diff;
+		}
+	}
+	
+	return dist;
+}
+
 double runSimulatedAnnealing(int mp[N]) {
 	double m = INF;
 	bool f[N];
 	int p[N];
-	double tmp;
+	double dist;
+	int count;
+	double diff;
+	bool update;
+	int noUpdate;
+	int n1, n2;
 	
 	for(int trial=0; trial<NUM_TRIALS; ++trial) {
-		int count = 0;
+		count = 0;
+		noUpdate = 0;
 		for(int i=0; i<N; ++i)
 			f[i] = false;
 		
 		dfs(randRange(N), &count, f, p);
-		tmp = calculateTravelDist(p);
+		dist = calculateTravelDist(p);
+		dist = untangle1(p, dist);
+		dist = untangle2(p, dist);
+		dist = untangle1(p, dist);
+		dist = untangle2(p, dist);
 		
-		if(tmp < m) {
-			m = tmp;
+		if(dist < m) {
+			m = dist;
 			for(int j=0; j<N; ++j)
 				mp[j] = p[j];
 		}
@@ -163,35 +212,83 @@ double runSimulatedAnnealing(int mp[N]) {
 		double t = TEMP_INIT;
 		
 		while(t > TEMP_FINAL) {
-			for(int i=0; i<NUM_ITERATIONS; ++i) {
-				int n1 = randRange(N);
-				int n2 = (n1+randRange(N-3)+2)%N;
+			update = false;
+			
+			for(int i=0; i<NUM_ITERATIONS_UNTANGLE; ++i) {
+				// Untangle phase
+				n1 = randRange(N);
+				n2 = (n1+randRange(N-3)+2)%N;
 				
 				if(n1 > n2)
 					swap(n1, n2);
 				
-				double diff = d[p[n2]*N+p[(n1+N-1)%N]] + d[p[n1]*N+p[(n2+1)%N]]
-						  - d[p[n1]*N+p[(n1+N-1)%N]] - d[p[n2]*N+p[(n2+1)%N]];
+				diff = d[p[n2]*N+p[(n1+N-1)%N]] + d[p[n1]*N+p[(n2+1)%N]]
+					 - d[p[n1]*N+p[(n1+N-1)%N]] - d[p[n2]*N+p[(n2+1)%N]];
 				
-//				printf("%f\n", diff);
 				if(diff < 0.0 || diff/m < 0.2 && exp(-diff/m/t)*double(RAND_MAX) > double(rand())) {
 					for(int j=0; j<(n2-n1+1)/2; ++j)
 						swap(p[n1+j], p[n2-j]);
 					
-					tmp += diff;
+					dist += diff;
 					
-					if(tmp < m) {
-						m = tmp;
+					if(dist < m) {
+						m = dist;
 						for(int j=0; j<N; ++j)
 							mp[j] = p[j];
 					}
+					
+					update = true;
 				}
 			}
+			
+			for(int i=0; i<NUM_ITERATIONS_INSERT; ++i) {
+				// Insert phase
+				n1 = randRange(N);
+				n2 = (n1+randRange(N-3)+2)%N;
+				
+				diff = d[p[(n1+N-1)%N]*N+p[(n1+1)%N]] + d[p[n2]*N+p[n1]] + d[p[n1]*N+p[(n2+1)%N]]
+					 - d[p[(n1+N-1)%N]*N+p[n1]] - d[p[n1]*N+p[(n1+1)%N]] - d[p[n2]*N+p[(n2+1)%N]];
+				
+				if(diff < 0.0 || diff/m < 0.2 && exp(-diff/m/t)*double(RAND_MAX) > double(rand())) {
+					if(n1 > n2)
+						n2 += N;
+					
+					int tmp = p[n1];
+					for(int j=n1; j<n2; ++j)
+						p[j%N] = p[(j+1)%N];
+					p[n2%N] = tmp;
+					
+					dist += diff;
+					
+					if(dist < m) {
+						m = dist;
+						for(int j=0; j<N; ++j)
+							mp[j] = p[j];
+					}
+					
+					update = true;
+				}
+			}
+			
+			if(update) {
+				noUpdate = 0;
+				dist = untangle1(p, dist);
+				dist = untangle2(p, dist);
+				
+				if(dist < m) {
+					m = dist;
+					for(int j=0; j<N; ++j)
+						mp[j] = p[j];
+				}
+			}
+			else
+				if(++noUpdate > CUT)
+					break;
 			
 			t *= TEMP_MULT;
 		}
 		
-		printf("Trial %d: %f (min: %f)\n", trial, tmp, m);
+		printf("Trial %d: %f (min: %f), temp: %f - %f\n", trial, dist, m, -log10(t), getElapsedTime());
 //		displayPath(p);
 	}
 	
@@ -199,6 +296,7 @@ double runSimulatedAnnealing(int mp[N]) {
 }
 
 int main() {
+	start = clock();
 	srand(time(NULL));
 	readData();
 	calculateMST();
