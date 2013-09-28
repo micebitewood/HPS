88227a7f3d90078e6742631a4cf54e19ff8b8e8d
micebitewood <mujiong@gmail.com>
2013-09-28 14:56:55 -0400

    add randomAdding.py

--- NoTipping/AB.py
+++ NoTipping/AB.py
@@ -61,19 +61,25 @@ def traversal(score, red, total, isRedTurn, lastPosition):
 #total = {1:4, 2:3, 6:1, 7:2, 8:5, 11:3, 14:10, 16:3, 19:4, 21:5}
 #board = [0, 4, 3, 0, 0, 0, 1, 2, 5, 0, 0, 3, 0, 0, 10, 0, 3, 0, 0, 4, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 #score = [-1, 85]
-red = {11:3, 12:3, 13:5, 14:8, 16:7, 18:9, 19:10, 20:1, 23:6, 25:4, 27:2} #including the green 3 in position -4(which is 11 here, because the board starts from -15 and our list starts from 0)
-total = {0:1, 1:2, 2:3, 3:4, 4:5, 5:6, 6:7, 7:8, 8:9, 11:3, 12:3, 13:5, 14:8, 16:7, 18:9, 19:10, 20:1, 23:6, 25:4, 27:2, 30:10} #including red, green, and blue
-score = [-215, 17]
+#red = {11:3, 12:3, 13:5, 14:8, 16:7, 18:9, 19:10, 20:1, 23:6, 25:4, 27:2} #including the green 3 in position -4(which is 11 here, because the board starts from -15 and our list starts from 0)
+#total = {0:1, 1:2, 2:3, 3:4, 4:5, 5:6, 6:7, 7:8, 8:9, 11:3, 12:3, 13:5, 14:8, 16:7, 18:9, 19:10, 20:1, 23:6, 25:4, 27:2, 30:10} #including red, green, and blue
+#score = [-215, 17]
 #red = {11:3, 16:3, 17:8, 18:9, 19:10, 20:7, 21:2, 23:6, 26:5}
 #total = {0:1, 1:2, 2:3, 3:4, 4:5, 5:6, 6:7, 7:8, 11:3, 16:3, 17:8, 18:9, 19:10, 20:7, 21:2, 23:6, 26:5}
 #board = [1, 2, 3, 4, 5, 6, 7, 8, 0, 0, 0, 3, 0, 0, 0, 0, 3, 8, 9, 10, 7, 2, 0, 6, 0, 0, 5, 0, 0, 0, 0]
 #score = [-128, 56]
 #red = {11:3, 15:6}
 #total = {9:3, 11:3, 15:6}
-#board = [0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 3, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 #score = [-15, 15]
 #red = {11:3, 15:6, 18:3}
 #total = {6:6, 10:3, 11:3, 15:6, 18:3}
 #board = [0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 3, 3, 0, 0, 0, 6, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 #score = [0, 48]
-print traversal(score, red, total, True, 0)
+with open('input', 'r') as f:
+    red = eval(f.readline())
+    total = eval(f.readline())
+    score = eval(f.readline())
+    print "red:", red
+    print "total:", total
+    print "score:", score
+    print traversal(score, red, total, True, 0)
---
+++ NoTipping/randomAdding.py
@@ -0,0 +1,73 @@
+def incScore(score, position, weight):
+    score[0] += weight * (12 - position)
+    score[1] += weight * (14 - position)
+
+def valid(score, position, weight):
+    newScore = score[:]
+    incScore(newScore, position, weight)
+    if newScore[0] < 0 and newScore[1] > 0:
+        return True
+    return False
+
+def getPossibleWeights(weights, total, score):
+    possibleWeights = []
+    for weight in range(1, 11):
+        if weight not in weights:
+            for position in range(31):
+                if position not in total.keys() and valid(score, position, weight):
+                    possibleWeights.append(weight)
+                    break
+    return possibleWeights
+
+def addPhase(red, redWeights, total, blueWeights, score, isRedTurn, count):
+    if count == 21:
+        print red
+        print total
+        print score
+        return True
+    if isRedTurn:
+        flag = True
+        possibleWeights = getPossibleWeights(redWeights, total, score)
+        if len(possibleWeights) == 0:
+            return False
+        while flag or not addPhase(newRed, newRedWeights, newTotal, blueWeights, newScore, False, count + 1):
+            flag = False
+            weightRed = possibleWeights[random.randint(0, len(possibleWeights) - 1)]
+            position = random.randint(0, 30)
+            while (position in total.keys()) or (not valid(score, position, weightRed)):
+                position = random.randint(0, 30)
+            newRed = red.copy()
+            newRed[position] = weightRed
+            newRedWeights = redWeights.copy()
+            newRedWeights.add(weightRed)
+            newTotal = total.copy()
+            newTotal[position] = weightRed
+            newScore = score[:]
+            incScore(newScore, position, weightRed)
+    else:
+        flag = True
+        possibleWeights = getPossibleWeights(blueWeights, total, score)
+        if len(possibleWeights) == 0:
+            return False
+        while flag or not addPhase(red, redWeights, newTotal, newBlueWeights, newScore, True, count + 1):
+            flag = False
+            weightBlue = possibleWeights[random.randint(0, len(possibleWeights) - 1)]
+            position = random.randint(0, 30)
+            while (position in total.keys()) or (not valid(score, position, weightBlue)):
+                position = random.randint(0, 30)
+            newTotal = total.copy()
+            newTotal[position] = weightBlue
+            newBlueWeights = blueWeights.copy()
+            newBlueWeights.add(weightBlue)
+            newScore = score[:]
+            incScore(newScore, position, weightBlue)
+    return True
+
+red = {11:3}
+redWeights = set()
+total = {11:3}
+blueWeights = set()
+score = [-6, 6]
+count = 1
+import random
+addPhase(red, redWeights, total, blueWeights, score, True, count)
