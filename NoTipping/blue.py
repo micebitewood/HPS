ac21d7977e9d4aa9f447c67320aa69a401591683
micebitewood <mujiong@gmail.com>
2013-09-28 22:16:34 -0400

    implement adding part

--- NoTipping/blue.py
+++ NoTipping/blue.py
@@ -6,7 +6,14 @@ def incScore(score, position, weight):
     score[0] -= weight * (3 + position)
     score[1] -= weight * (1 + position)
 
-def valid(score, position, weight):
+def moveValid(score, position, weight):
+    newScore = score[:]
+    incScore(newScore, position, weight)
+    if newScore[0] < 0 and newScore[1] > 0:
+        return True
+    return False
+
+def removeValid(score, position, weight):
     newScore = score[:]
     decScore(newScore, position, weight)
     if newScore[0] < 0 and newScore[1] > 0:
@@ -18,7 +25,7 @@ def traversal(score, red, total, isRedTurn, lastPosition):
         if len(red) != 0:
             for position in red.keys():
                 weight = red[position]
-                if valid(score, position, weight):
+                if removeValid(score, position, weight):
                     newScore = score[:]
                     decScore(newScore, position, weight)
                     newTotal = total.copy()
@@ -31,7 +38,7 @@ def traversal(score, red, total, isRedTurn, lastPosition):
         else:
             for position in total.keys():
                 weight = total[position]
-                if valid(score, position, weight):
+                if removeValid(score, position, weight):
                     newScore = score[:]
                     decScore(newScore, position, weight)
                     newTotal = total.copy()
@@ -43,7 +50,7 @@ def traversal(score, red, total, isRedTurn, lastPosition):
     else:
         for position in total.keys():
             weight = total[position]
-            if valid(score, position, weight):
+            if removeValid(score, position, weight):
                 newScore = score[:]
                 decScore(newScore, position, weight)
                 newTotal = total.copy()
@@ -56,13 +63,13 @@ def traversal(score, red, total, isRedTurn, lastPosition):
                     return (False, position)
         return (True, 0)
         
-def getNextMove(total, red):
+def getNextRemove(total, red):
     score = [0, 0]
     for position in total.keys():
         incScore(score, position, total[position])
     for position in total.keys():
         weight = total[position]
-        if valid(score, position, weight):
+        if removeValid(score, position, weight):
             newScore = score[:]
             decScore(newScore, position, weight)
             newTotal = total.copy()
@@ -76,6 +83,39 @@ def getNextMove(total, red):
     for position in total.keys():
         return (position, total[position])
 
+def getNextMove(total, red):
+    score = [0, 0]
+    for position in total.keys():
+        incScore(score, position, total[position])
+    if -3 not in total.keys():
+        for weight in range(12, 0, -1):
+            if weight not in blueWeights:
+                return (-3, weight)
+    elif -2 not in total.keys():
+        for weight in range(12, 0, -1):
+            if weight not in blueWeights:
+                return (-2, weight)
+    elif -1 not in total.keys():
+        for weight in range(12, 0, -1):
+            if weight not in blueWeights:
+                return (-1, weight)
+    elif -score[0] > score[1]:
+        for weight in range(1, 13):
+            if weight not in blueWeights:
+                for position in range(31):
+                    position -= 15
+                    if position not in total.keys():
+                        if moveValid(score, position, weight): 
+                            return (position, weight)
+    else:
+        for weight in range(1, 13):
+            if weight not in blueWeights:
+                for position in range(31, 0, -1):
+                    position -= 16
+                    if position not in total.keys():
+                        if moveValid(score, position, weight):
+                            return (position, weight)
+    
 import sys
 mode = eval(sys.argv[1])
 player = eval(sys.argv[2])
@@ -83,6 +123,8 @@ remainingTime = eval(sys.argv[3])
 
 total = dict()
 red = dict()
+redWeights = set()
+blueWeights = set()
 with open('board.txt', 'r') as f:
     for line in f:
         temp = map(int, line.split())
@@ -90,5 +132,12 @@ with open('board.txt', 'r') as f:
             total[temp[0]] = temp[1]
             if temp[2] < 2:
                 red[temp[0]] = temp[1]
-(position, weight) = getNextMove(total, red)
-print position, weight
+                redWeights.add(temp[1])
+            else:
+                blueWeights.add(temp[1])
+if mode == 1:
+    (position, weight) = getNextMove(total, red)
+    print position, weight
+if mode == 2:
+    (position, weight) = getNextRemove(total, red)
+    print position, weight
--- NoTipping/red.py
+++ NoTipping/red.py
@@ -6,7 +6,14 @@ def incScore(score, position, weight):
     score[0] -= weight * (3 + position)
     score[1] -= weight * (1 + position)
 
-def valid(score, position, weight):
+def moveValid(score, position, weight):
+    newScore = score[:]
+    incScore(newScore, position, weight)
+    if newScore[0] < 0 and newScore[1] > 0:
+        return True
+    return False
+
+def removeValid(score, position, weight):
     newScore = score[:]
     decScore(newScore, position, weight)
     if newScore[0] < 0 and newScore[1] > 0:
@@ -18,7 +25,7 @@ def traversal(score, red, total, isRedTurn, lastPosition):
         if len(red) != 0:
             for position in red.keys():
                 weight = red[position]
-                if valid(score, position, weight):
+                if removeValid(score, position, weight):
                     newScore = score[:]
                     decScore(newScore, position, weight)
                     newTotal = total.copy()
@@ -31,7 +38,7 @@ def traversal(score, red, total, isRedTurn, lastPosition):
         else:
             for position in total.keys():
                 weight = total[position]
-                if valid(score, position, weight):
+                if removeValid(score, position, weight):
                     newScore = score[:]
                     decScore(newScore, position, weight)
                     newTotal = total.copy()
@@ -43,7 +50,7 @@ def traversal(score, red, total, isRedTurn, lastPosition):
     else:
         for position in total.keys():
             weight = total[position]
-            if valid(score, position, weight):
+            if removeValid(score, position, weight):
                 newScore = score[:]
                 decScore(newScore, position, weight)
                 newTotal = total.copy()
@@ -56,7 +63,7 @@ def traversal(score, red, total, isRedTurn, lastPosition):
                     return (False, position)
         return (True, 0)
         
-def getNextMove(total, red):
+def getNextRemove(total, red):
     score = [0, 0]
     for position in total.keys():
         incScore(score, position, total[position])
@@ -64,7 +71,7 @@ def getNextMove(total, red):
     if len(red) != 0:
         for position in red.keys():
             weight = red[position]
-            if valid(score, position, weight):
+            if removeValid(score, position, weight):
                 newScore = score[:]
                 decScore(newScore, position, weight)
                 newTotal = total.copy()
@@ -79,7 +86,7 @@ def getNextMove(total, red):
     else:
         for position in total.keys():
             weight = total[position]
-            if valid(score, position, weight):
+            if removeValid(score, position, weight):
                 newScore = score[:]
                 decScore(newScore, position, weight)
                 newTotal = total.copy()
@@ -90,6 +97,38 @@ def getNextMove(total, red):
         for position in total.keys():
             return (position, total[position])
 
+def getNextMove(total, red):
+    score = [0, 0]
+    for position in total.keys():
+        incScore(score, position, total[position])
+    if -3 not in total.keys():
+        for weight in range(12, 0, -1):
+            if weight not in redWeights:
+                return (-3, weight)
+    elif -2 not in total.keys():
+        for weight in range(12, 0, -1):
+            if weight not in redWeights:
+                return (-2, weight)
+    elif -1 not in total.keys():
+        for weight in range(12, 0, -1):
+            if weight not in redWeights:
+                return (-1, weight)
+    elif -score[0] > score[1]:
+        for weight in range(1, 13):
+            if weight not in redWeights:
+                for position in range(31):
+                    position -= 15
+                    if position not in total.keys():
+                        if moveValid(score, position, weight): 
+                            return (position, weight)
+    else:
+        for weight in range(1, 13):
+            if weight not in redWeights:
+                for position in range(31, 0, -1):
+                    position -= 16
+                    if position not in total.keys():
+                        if moveValid(score, position, weight):
+                            return (position, weight)
 import sys
 mode = eval(sys.argv[1])
 player = eval(sys.argv[2])
@@ -97,6 +136,8 @@ remainingTime = eval(sys.argv[3])
 
 total = dict()
 red = dict()
+redWeights = set()
+blueWeights = set()
 with open('board.txt', 'r') as f:
     for line in f:
         temp = map(int, line.split())
@@ -104,6 +145,12 @@ with open('board.txt', 'r') as f:
             total[temp[0]] = temp[1]
             if temp[2] < 2:
                 red[temp[0]] = temp[1]
-(position, weight) = getNextMove(total, red)
-print position, weight
-
+                redWeights.add(temp[1])
+            else:
+                blueWeights.add(temp[1])
+if mode == 1:
+    (position, weight) = getNextMove(total, red)
+    print position, weight
+elif mode == 2:
+    (position, weight) = getNextRemove(total, red)
+    print position, weight
