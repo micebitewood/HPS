187bcd96056a29af028e0ea9aa434c0a777b2ebb
micebitewood <mujiong@gmail.com>
2013-11-08 13:59:03 -0500

    DatingGame server and random player

---
+++ DatingGame/DatingGame.java
@@ -0,0 +1,199 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class DatingGame {
+    
+    private static final String EOM = "<EOM>";
+    private static final double MAX_PERCENT_MODIFICATION = 0.20;
+    private static final double MAX_NUM_MODIFICATIONS = 0.05;
+    
+    private static PrintWriter out = null;
+    private static BufferedReader in = null;
+    private static int port;
+    private static final String host = "127.0.0.1";
+    private static final String teamName = "JJ";
+    
+    private static int numFeatures;
+    
+    public static void main(String[] args) throws IOException {
+        
+        port = Integer.parseInt(args[0]);
+        Socket sock = new Socket(host, port);
+        out = new PrintWriter(sock.getOutputStream());
+        in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+        
+        String question = readData(in);
+        if (!question.equals("Team Name?")) {
+            System.err.println("Server not following convention.");
+            System.exit(0);
+        }
+        
+        sendSocket(out, teamName);
+        String[] startItems = (readData(in).trim()).split("\n");
+        String[] modeAndNum = startItems[0].split("\\s+");
+        String mode = modeAndNum[0];
+        numFeatures = Integer.parseInt(modeAndNum[1]);
+        
+        if (mode.equals("P")) {
+            Person person = new Person();
+            sendSocket(out, person.genWeights(numFeatures));
+            
+            while (!readData(in).equals("end")) {
+                sendSocket(out, person.fixWeights());
+            }
+        } else if (mode.equals("M")) {
+            Matchmaker matchmaker = new Matchmaker(startItems, numFeatures);
+            do {
+                sendSocket(out, matchmaker.getNextCandidates());
+            } while (!(matchmaker.getData(readData(in))).equals("end"));
+            System.out.println("end");
+        }
+        in.close();
+        out.close();
+        sock.close();
+    }
+    
+    /* Socket Functions */
+    
+    private static String readData(BufferedReader in) throws IOException {
+        String chunk = null;
+        StringBuilder data = new StringBuilder();
+        
+        while ((chunk = in.readLine()) == null) {
+        }
+        
+        do {
+            data.append(chunk).append("\n");
+            
+            int lastIndex = data.toString().indexOf(EOM);
+            if (lastIndex >= 0) {
+                return data.toString().substring(0, lastIndex);
+            }
+        } while ((chunk = in.readLine()) != null);
+        
+        return null;
+    }
+    
+    private static void sendSocket(PrintWriter out, String msg) {
+        msg += EOM;
+        System.out.println("send: " + msg);
+        out.println(msg);
+        out.flush();
+    }
+    /* End of Socket Functions */
+    
+}
+
+class Person {
+    List<Integer> weights;
+    int numFeatures;
+    
+    public String genWeights(int numFeatures) {
+        weights = new ArrayList<Integer>();
+        this.numFeatures = numFeatures;
+        Random random = new Random(System.currentTimeMillis());
+        int posNum = random.nextInt(numFeatures + 1);
+        int sum = 0;
+        for (int i = 0; i < posNum - 1; i++) {
+            int next = random.nextInt(101 - sum);
+            weights.add(next);
+            sum += next;
+            System.out.println("pos: " + next + " sum: " + sum);
+        }
+        if (posNum != 0) {
+            weights.add(100 - sum);
+        }
+        sum = 0;
+        for (int i = posNum; i < numFeatures - 1; i++) {
+            int next = -random.nextInt(101 - sum);
+            weights.add(next);
+            sum -= next;
+            System.out.println("neg: " + next + " sum: " + sum);
+        }
+        if (posNum != numFeatures) {
+            weights.add(sum - 100);
+        }
+        Collections.shuffle(weights);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < numFeatures; i++) {
+            if (weights.get(i) >= 0) {
+                if (weights.get(i) == 100)
+                    sb.append("1.00 ");
+                else
+                    sb.append(String.format("0.%02d ", weights.get(i)));
+            }
+            else {
+                if (weights.get(i) == -100)
+                    sb.append("-1.00 ");
+                else
+                    sb.append(String.format("-0.%02d ", -weights.get(i)));
+            }
+        }
+        System.out.println();
+        return sb.toString().trim();
+    }
+    
+    public String fixWeights() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < numFeatures; i++) {
+            if (weights.get(i) >= 0) {
+                if (weights.get(i) == 100)
+                    sb.append("1.00 ");
+                else
+                    sb.append(String.format("0.%02d ", weights.get(i)));
+            }
+            else {
+                if (weights.get(i) == -100)
+                    sb.append("-1.00 ");
+                else
+                    sb.append(String.format("-0.%02d ", -weights.get(i)));
+            }
+        }
+        return sb.toString().trim();
+    }
+    
+}
+
+class Matchmaker {
+    double[][] candidates;
+    double[] scores;
+    int numFeatures;
+    
+    public Matchmaker(String[] initString, int numFeatures) {
+        candidates = new double[40][numFeatures];
+        scores = new double[40];
+        this.numFeatures = numFeatures;
+        for (int i = 0; i < 20; i++) {
+            String[] candidatesAndScores = initString[i + 1].split("\\s+");
+            for (int j = 0; j < numFeatures; j++) {
+                candidates[i][j] = Double.parseDouble(candidatesAndScores[j]);
+            }
+            scores[i] = Double.parseDouble(candidatesAndScores[numFeatures]);
+        }
+    }
+    
+    public String getNextCandidates() {
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random(System.currentTimeMillis());
+        for (int i = 0; i < numFeatures; i++) {
+            sb.append(random.nextDouble() + " ");
+        }
+        return sb.toString().trim();
+    }
+    
+    public String getData(String readData) {
+        String[] lines = readData.split("\n");
+        int lastInd = lines.length - 1;
+        String[] candidatesAndScores = lines[lastInd].split("\\s+");
+        scores[lastInd] = Double.parseDouble(candidatesAndScores[numFeatures]);
+        System.out.println("last score: " + scores[lastInd]);
+        return readData;
+    }
+}
